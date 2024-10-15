6fa2bb2e74709e07a51110fee047cdddec889def
Manuel Grundner <m.grundner@delegate.at>
2024-10-15 19:01:02 +0200

    feat: added front-end to backend publishing

--- Services/ConsumingTimedHostedService.cs
+++ Services/ConsumingTimedHostedService.cs
@@ -1,10 +1,19 @@
 ﻿
+using ChannelsPubSub.Protocol;
+
 namespace ChannelsPubSub.Services;
 
-public sealed class ConsumingTimedHostedService : BackgroundService
+public sealed class ConsumingTimedHostedService(
+    IPubSub<ClickTimeMessage> subscriber,
+    ILogger<ConsumingTimedHostedService> logger
+) : BackgroundService
 {
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        
+        while(await subscriber.Reader.WaitToReadAsync(stoppingToken))
+        {
+            var message = await subscriber.Reader.ReadAsync(stoppingToken);
+            logger.LogInformation("Received click time message: {DateTime:F}", message.DateTime.ToLocalTime());
+        }
     }
 }
