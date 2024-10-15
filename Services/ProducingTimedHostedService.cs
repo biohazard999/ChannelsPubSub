e9183629d748a476ca950da6777a1fc9422755e4
Manuel Grundner <m.grundner@delegate.at>
2024-10-15 18:54:11 +0200

    feat: added publish from background

--- Services/ProducingTimedHostedService.cs
+++ Services/ProducingTimedHostedService.cs
@@ -1,10 +1,17 @@
 ﻿
+using ChannelsPubSub.Protocol;
+
 namespace ChannelsPubSub.Services;
 
-public sealed class ProducingTimedHostedService : BackgroundService
+public sealed class ProducingTimedHostedService(IPubSub<BackgroundTimeMessage> publisher) : BackgroundService
 {
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+        while(await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            await publisher.Writer.WriteAsync(new BackgroundTimeMessage(DateTime.UtcNow), stoppingToken);
+        }
     }
 }
